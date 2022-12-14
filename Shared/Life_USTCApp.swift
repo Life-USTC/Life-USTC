065319e0d41575e2ffb1fc7926d6197b5f188347
tiankaima <tiankaima@mail.ustc.edu.cn>
2022-12-14 21:03:53 +0800

    Initial Commit

---
+++ Shared/Life_USTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_USTCApp.swift
+//  Shared
+//
+//  Created by TiankaiMa on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct Life_USTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
