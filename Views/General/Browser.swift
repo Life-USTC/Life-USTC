543ceaca419451daf585e1f1be77f17a874d10ec
tiankaima <tiankaima@mail.ustc.edu.cn>
2023-01-14 23:23:22 +0800

    Add ChangeLog

--- Views/General/Browser.swift
+++ Views/General/Browser.swift
@@ -12,7 +12,7 @@ struct SwiftUIWebView: UIViewRepresentable {
     typealias UIViewType = WKWebView
     let webView: WKWebView
     var url: URL
-    
+
     init(url: URL) {
         self.url = url
         let wkWebConfig = WKWebViewConfiguration()
@@ -25,21 +25,23 @@ struct SwiftUIWebView: UIViewRepresentable {
         // identify self as mobile client, so that the website will render the mobile version
         webView.customUserAgent = #"Mozilla/5.0 (iPod; CPU iPhone OS 12_0 like macOS) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/12.0 Mobile/14A5335d Safari/602.1.50"#
     }
-    func makeUIView(context: Context) -> WKWebView {
-        self.webView
+
+    func makeUIView(context _: Context) -> WKWebView {
+        webView
     }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        self.webView.load(URLRequest(url: url))
+
+    func updateUIView(_: WKWebView, context _: Context) {
+        webView.load(URLRequest(url: url))
     }
 }
 
 struct Browser: View {
     var url: URL
     var title: String = "Detail"
-    
+
     var body: some View {
         SwiftUIWebView(url: url)
-            .padding([.leading,.trailing],2)
+            .padding([.leading, .trailing], 2)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     ShareLink(item: self.url) {
