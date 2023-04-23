298973a98e365882b951c3c6ce4f1bc301c52382
tiankaima <tiankaima@163.com>
2023-04-23 14:02:13 +0800

    Update around TPNS features, fix version number and Appdelegate logic

--- Models/AppDelegate.swift
+++ Models/AppDelegate.swift
@@ -6,30 +6,63 @@
 //
 
 import SwiftUI
+import UIKit
 
 #if os(iOS)
 
-var tpnsLog: String = ""
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, XGPushDelegate, ObservableObject {
+    @Published var tpnsLog: String = ""
 
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, XGPushDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        XGPush.defaultManager().clearTPNSCache()
+        if userDefaults.bool(forKey: "useNotification") {
+            startTPNS()
+        }
+        return true
+    }
+    
+    func startTPNS() {
         XGPush.defaultManager().isEnableDebug = true
         XGPush.defaultManager().configureClusterDomainName("tpns.sh.tencent.com")
         XGPush.defaultManager().appDelegate = self
         XGPush.defaultManager().startXG(withAccessID: 1_680_015_447, accessKey: "IOSAEBOQD6US", delegate: self)
-        return true
+    }
+    
+    func stopTPNS() {
+        XGPush.defaultManager().stopXGNotification()
     }
 
-    func xgPushDidRegisteredDeviceToken(_: String?, xgToken _: String?, error _: Error?) {}
-
+    func xgPushDidRegisteredDeviceToken(_: String?, xgToken _: String?, error _: Error?) {
+        // When TPNS is started:
+    }
+    
+    func xgPushDidFinishStop(_ isSuccess: Bool, error: Error?) {
+        // When stop TPNS is requested, callback here
+    }
+    
     func xgPushDidReceiveRemoteNotification(_: Any) async -> UInt {
         1
+    }
+    
+    func xgPushDidRequestNotificationPermission(_ isEnable: Bool, error: Error?) {
+        if !isEnable || error != nil {
+            print("Request Notification failed")
+
+            DispatchQueue.main.async {
+                // sadly this isn't working as the startup page have multiple sheet before alert could show up
+                let controller = UIApplication.shared.windows.first?.rootViewController as? UIViewController
+                let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
+                controller?.present(alert, animated: true)
+            }
+        }
     }
 
     func xgPushLog(_ logInfo: String?) {
         if let logInfo {
-            tpnsLog += "\n" + logInfo
+            DispatchQueue.main.async {
+                self.tpnsLog += "\n" + logInfo
+                self.objectWillChange.send()
+            }
         }
     }
 }
--- Views/Settings/NotificationPage.swift
+++ Views/Settings/NotificationPage.swift
@@ -10,18 +10,31 @@ import UIKit
 
 #if os(iOS)
 struct NotificationSettingView: View {
+    @EnvironmentObject var appDelegate: AppDelegate // This isn't working on preview devices for some reason, ignore this for now before we determine what's not working.
     @AppStorage("useNotification", store: userDefaults) var useNotification = true
     @State private var showingAlert = false
-
+    
     var body: some View {
         List {
+#if DEBUG
+#if arch(arm64)
+            // ensure that this isn't shown on preview device
+            // as it runs off rossetta (apple chips) so that it's always x86_64
+            ScrollView {
+                Text(appDelegate.tpnsLog)
+            }
+            .border(.yellow)
+            .frame(height: 150)
+            .padding(0)
+#endif
+#endif
             Section {
                 Toggle("Allow Notification", isOn: $useNotification)
                     .onChange(of: useNotification) { newValue in
                         if newValue {
-                            XGPush.defaultManager().startXG(withAccessID: 1_680_015_447, accessKey: "IOSAEBOQD6US", delegate: nil)
+                            appDelegate.startTPNS()
                         } else {
-                            XGPush.defaultManager().stopXGNotification()
+                            appDelegate.stopTPNS()
                         }
                     }
 
@@ -38,9 +51,16 @@ struct NotificationSettingView: View {
                 Text("General")
                     .textCase(.none)
             }
-
+            
 #if DEBUG
-            Section {} header: {
+            Section {   
+                
+                Button {
+                    XGPushTokenManager.default().appendTags([])
+                } label: {
+                    Text("Update Labels")
+                }
+            } header: {
                 Text("Labels")
                     .textCase(.none)
             }
@@ -55,6 +75,7 @@ struct NotificationSetting_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             NotificationSettingView()
+                .environmentObject(AppDelegate())
         }
     }
 }
