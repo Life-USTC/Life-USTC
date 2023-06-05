792ade0f7c04a2cd81a4310921ecd6f80584406a
tiankaima <tiankaima@163.com>
2023-06-05 14:02:51 +0800

    Add more feed sources

--- General/Constants.swift
+++ General/Constants.swift
@@ -13,6 +13,11 @@ let appFeedURL = URL(string: "https://tiankaima.github.io/Life-USTC-Web/notices/
 let ustcLoginUrl = URL(string: "https://passport.ustc.edu.cn/login")!
 let ustcCasUrl = URL(string: "https://passport.ustc.edu.cn")!
 let exampleURL = URL(string: "https://example.com")!
-let mp_ustc_official_URL = URL(string: "https://tiankaima.github.io/rss-backup/xml/%E5%85%AC%E4%BC%97%E5%8F%B7%E3%80%8C%E4%B8%AD%E5%9B%BD%E7%A7%91%E5%AD%A6%E6%8A%80%E6%9C%AF%E5%A4%A7%E5%AD%A6%E3%80%8D.xml")!
+let mp_ustc_official_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_main.xml")!
+let mp_ustc_graduate_student_union_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_graduate_student_union.xml")!
+let mp_ustc_youth_league_committee_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_youth_league_committee.xml")!
+let mp_ustc_undergraduate_admission_office_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_undergraduate_admission_office.xml")!
+let mp_ustc_sgy_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_sgy.xml")!
+let mp_ustc_undergraduate_student_union_URL = URL(string: "https://rss-cdn.tiankaima.dev/xml/mp_ustc_undergraduate_student_union.xml")!
 
 let userDefaults = UserDefaults(suiteName: "group.dev.tiankaima.Life-USTC")!
