b72d07614df5a9e2931ed8f41a43c193cd220e90
tiankaima <tiankaima@163.com>
2023-08-17 15:36:53 +0800

    fix: remove TimePeroid

--- Views/General/Feed/FeedVStackView.swift
+++ Views/General/Feed/FeedVStackView.swift
@@ -9,37 +9,6 @@ import SwiftUI
 
 struct FeedVStackView: View {
     var feeds: [Feed]
-    var postsSorted: [TimePeroid: [Feed]] {
-        var result: [TimePeroid: [Feed]] = [.day: [], .week: [], .month: [], .year: [], .longerThanAYear: []]
-        for post in feeds {
-            for timePeroid in TimePeroid.allCases {
-                if timePeroid.dateRange.contains(post.datePosted) {
-                    result[timePeroid]!.append(post)
-                }
-            }
-        }
-        return result
-    }
-
-    @ViewBuilder func makeView(_ key: TimePeroid, _ value: [Feed]) -> some View {
-        if !value.isEmpty {
-            VStack {
-                VStack(alignment: .leading) {
-                    Text(key.dateRangeCaption)
-                        .foregroundColor(.secondary)
-                        .fontWeight(.semibold)
-                    Text(key.caption)
-                        .font(.title2)
-                        .bold()
-                }
-                .hStackLeading()
-                ForEach(value, id: \.id) { post in
-                    FeedView(feed: post)
-                }
-            }
-            .padding(.bottom, 40)
-        }
-    }
 
     var body: some View {
         List {
