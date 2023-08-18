7bd61cbf951fcc6aee9c2f4eae11ab30d309061b
tiankaima <tiankaima@163.com>
2023-08-18 14:08:30 +0800

    refactor: clean files

--- Models/School/Exam.swift
+++ Models/School/Exam.swift
@@ -6,9 +6,7 @@
 //
 
 import EventKit
-import SwiftSoup
 import SwiftUI
-import WidgetKit
 
 struct Exam: Codable, Equatable {
     // MARK: - Information about the course
@@ -50,6 +48,18 @@ extension Exam {
         "\(startDate.description(with: .current)) - \(endDate.description(with: .current)) @ \(classRoomName)"
     }
 
+    var isFinished: Bool {
+        endDate <= Date()
+    }
+
+    var daysLeft: Int {
+        Calendar.current.dateComponents([.day],
+                                        from: .now.stripTime(),
+                                        to: startDate.stripTime()).day ?? 0
+    }
+}
+
+extension Exam {
     static func clean(_ exams: [Exam]) -> [Exam] {
         let hiddenExamName = ([String].init(rawValue: UserDefaults.appGroup.string(forKey: "hiddenExamName") ?? "") ?? []).filter { !$0.isEmpty }
         let result = exams.filter { exam in
@@ -69,6 +79,68 @@ extension Exam {
             return false
         }
         return Exam.show(result) + Exam.show(hiddenResult)
+    }
+
+    /// Sort given exams by time(ascending), and put the ones that are already over to the end of the array
+    static func show(_ exams: [Exam]) -> [Exam] {
+        exams
+            .filter { !$0.isFinished }
+            .sorted { $0.startDate < $1.endDate }
+            + exams
+            .filter(\.isFinished)
+            .sorted { $0.startDate > $1.endDate }
+    }
+
+    /// Merge two list of exam (addition only)
+    static func merge(_ original: [Exam], with new: [Exam]) -> [Exam] {
+        var result = original
+        for exam in new {
+            if !result.filter({ $0 == exam }).isEmpty {
+                continue
+            }
+            result.append(exam)
+        }
+        return result
+    }
+}
+
+extension Exam {
+    /// Convert to EKEvent
+    func event(in store: EKEventStore = EKEventStore()) -> EKEvent {
+        let event = EKEvent(eventStore: store)
+        event.title = courseName + " " + typeName
+        event.location = classRoomName + "@" + classRoomBuildingName
+        event.notes = description
+
+        event.startDate = startDate
+        event.endDate = endDate
+        return event
+    }
+
+    static func saveToCalendar(_ exams: [Exam]) async throws {
+        let eventStore = EKEventStore()
+        if try await !eventStore.requestAccess(to: .event) {
+            throw BaseError.runtimeError("Calendar access problem")
+        }
+
+        let calendarName = "Upcoming Exams"
+        var calendar: EKCalendar? = eventStore.calendars(for: .event).first(where: { $0.title == calendarName.localized })
+
+        // try remove everything with that name in it
+        if calendar != nil {
+            try eventStore.removeCalendar(calendar!, commit: true)
+        }
+
+        calendar = EKCalendar(for: .event, eventStore: eventStore)
+        calendar!.title = calendarName
+        calendar!.cgColor = Color.accentColor.cgColor
+        calendar!.source = eventStore.defaultCalendarForNewEvents?.source
+        try! eventStore.saveCalendar(calendar!, commit: true)
+
+        for exam in exams {
+            try eventStore.save(exam.event(), span: .thisEvent, commit: false)
+        }
+        try eventStore.commit()
     }
 }
 
