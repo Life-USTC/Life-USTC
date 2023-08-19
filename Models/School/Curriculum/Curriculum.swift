62d3e5f0913bbe639dae9d0ba2afb0c23dfeeb21
tiankaima <tiankaima@163.com>
2023-08-19 19:18:29 +0800

    feat: update CurriculumProtocol definition

--- Models/School/Curriculum/Curriculum.swift
+++ Models/School/Curriculum/Curriculum.swift
@@ -15,11 +15,16 @@ struct Curriculum: Codable {
 }
 
 protocol CurriculumProtocol {
+    func refresh() async throws -> Curriculum
+}
+
+/// Usage: `class exampleDelegaet: CurriculumProtocolA & CurriculumProtocol`
+protocol CurriculumProtocolA {
     func refreshSemesterList() async throws -> [String: String]
     func refreshSemester(id: String) async throws -> Semester
 }
 
-extension CurriculumProtocol {
+extension CurriculumProtocolA {
     /// Parrallel refresh the whole curriculum
     func refresh() async throws -> Curriculum {
         var result = Curriculum(semesters: [], semesterList: [:])
@@ -28,6 +33,36 @@ extension CurriculumProtocol {
             for id in result.semesterList.keys {
                 group.addTask {
                     try? await self.refreshSemester(id: id)
+                }
+            }
+
+            for await child in group {
+                if let child {
+                    result.semesters.append(child)
+                }
+            }
+        }
+        return result
+    }
+}
+
+/// - Note: Useful when semester startDate is not provided in `refreshSemesterList`
+protocol CurriculumProtocolB {
+    /// Return more info than just id and name, like start date and end date, but have empty courses
+    func refreshSemesterBase() async throws -> [Semester]
+    func refreshSemester(inComplete: Semester) async throws -> Semester
+}
+
+extension CurriculumProtocolB {
+    /// Parrallel refresh the whole curriculum
+    func refresh() async throws -> Curriculum {
+        var result = Curriculum(semesters: [], semesterList: [:])
+        let incompleteSemesters = try await refreshSemesterBase()
+        result.semesterList = incompleteSemesters.reduce(into: [:]) { $0[$1.id] = $1.name }
+        await withTaskGroup(of: Semester?.self) { group in
+            for semester in incompleteSemesters {
+                group.addTask {
+                    try? await self.refreshSemester(inComplete: semester)
                 }
             }
 
--- Schools/USTC/Delegates/USTC+CurriculumDelegate.swift
+++ Schools/USTC/Delegates/USTC+CurriculumDelegate.swift
@@ -10,17 +10,44 @@ import SwiftUI
 import SwiftyJSON
 import WidgetKit
 
-class USTCCurriculumDelegate: CurriculumProtocol {
+class USTCCurriculumDelegate: CurriculumProtocolB & CurriculumProtocol {
     static let shared = USTCCurriculumDelegate()
 
     @LoginClient(\.ustcUgAAS) var ugAASClient: UstcUgAASClient
     @LoginClient(\.ustcCatalog) var catalogClient: UstcCatalogClient
 
-    func refreshSemesterList() async throws -> [String: String] {
-        [:]
+    func refreshSemesterBase() async throws -> [Semester] {
+        func convertYYMMDD(_ date: String) -> Date {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            return dateFormatter.date(from: date)!
+        }
+
+        if try await !_catalogClient.requireLogin() {
+            throw BaseError.runtimeError("UstcCatalog Not logined")
+        }
+        let validToken = catalogClient.token
+
+        let url = URL(string: "https://catalog.ustc.edu.cn/api/teach/semester/list?access_token=\(validToken)")!
+
+        var request = URLRequest(url: url)
+        request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let json = try JSON(data: data)
+
+        var result: [Semester] = []
+        for (_, subJson) in json {
+            result.append(Semester(id: subJson["id"].stringValue,
+                                   courses: [],
+                                   name: subJson["nameZh"].stringValue,
+                                   startDate: convertYYMMDD(subJson["startDate"].stringValue),
+                                   endDate: convertYYMMDD(subJson["endDate"].stringValue)))
+        }
+
+        return result
     }
 
-    func refreshSemester(id: String) async throws -> Semester {
+    func refreshSemester(inComplete: Semester) async throws -> Semester {
         let queryURL = URL(string: "https://jw.ustc.edu.cn/for-std/course-table")!
         if try await !_ugAASClient.requireLogin() {
             throw BaseError.runtimeError("UstcUgAAS Not logined")
@@ -38,7 +65,7 @@ class USTCCurriculumDelegate: CurriculumProtocol {
             }
         }
 
-        let url = URL(string: "https://jw.ustc.edu.cn/for-std/course-table/semester/\(id)/print-data/\(tableID)?weekIndex=")!
+        let url = URL(string: "https://jw.ustc.edu.cn/for-std/course-table/semester/\(inComplete.id)/print-data/\(tableID)?weekIndex=")!
         request = URLRequest(url: url)
         request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
         let (data, _) = try await URLSession.shared.data(for: request)
