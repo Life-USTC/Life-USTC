30ee831fd48287aa22e24205d4fc500de5c9508c
tiankaima <tiankaima@163.com>
2023-08-23 02:33:07 +0800

    chore: rename and move files

---
+++ Models/ManagedData/ManagedDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  ManagedDataSource.swift
+//  Life@USTC
+//
+//  Created by Tiankai Ma on 2023/8/18.
+//
+
+import Foundation
+
+class ManagedLocalDataProtocol<D>: ObservableObject {
+    var data: D?
+    var status: LocalAsyncStatus {
+        .notFound
+    }
+
+    init(data: D? = nil) {
+        self.data = data
+    }
+}
+
+protocol ManagedRemoteUpdateProtocol<D> {
+    associatedtype D
+
+    func refresh() async throws -> D
+}
+
+struct ManagedDataSource<D> {
+    var local: ManagedLocalDataProtocol<D>
+    var remote: any ManagedRemoteUpdateProtocol<D>
+}
