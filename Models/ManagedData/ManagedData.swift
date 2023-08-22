0cca1efaf02d55be93148be7ef1ea9a6f76997d2
tiankaima <tiankaima@163.com>
2023-08-23 02:33:07 +0800

    chore: rename and move files

--- Models/ManagedData/ManagedData.swift
+++ Models/ManagedData/ManagedData.swift
@@ -5,26 +5,49 @@
 //  Created by Tiankai Ma on 2023/8/18.
 //
 
-import Foundation
+import SwiftUI
 
-class ManagedLocalDataProtocol<D>: ObservableObject {
-    var data: D?
-    var status: LocalAsyncStatus {
-        .notFound
-    }
-
-    init(data: D? = nil) {
-        self.data = data
-    }
-}
-
-protocol ManagedRemoteUpdateProtocol<D> {
-    associatedtype D
-
-    func refresh() async throws -> D
-}
-
-struct ManagedDataSource<D> {
-    var local: ManagedLocalDataProtocol<D>
+@propertyWrapper
+struct ManagedData<D>: DynamicProperty {
+    @ObservedObject var local: ManagedLocalDataProtocol<D>
     var remote: any ManagedRemoteUpdateProtocol<D>
+
+    var wrappedValue: D? {
+        if local.status != .valid {
+            triggerRefresh()
+        }
+        return local.data
+    }
+
+    @State var refresh: RefreshAsyncStatus? = nil
+    var status: AsyncStatus {
+        AsyncStatus(
+            local: local.status,
+            refresh: refresh
+        )
+    }
+
+    func retrive() async throws -> D? {
+        if status.local != .valid {
+            try await refresh()
+        }
+        return wrappedValue
+    }
+
+    func refresh() async throws {
+        try await $refresh.exec {
+            local.data = try await remote.refresh()
+        }
+    }
+
+    func triggerRefresh() {
+        Task { @MainActor in
+            try await self.refresh()
+        }
+    }
+
+    init(_ source: ManagedDataSource<D>) {
+        _local = .init(wrappedValue: source.local)
+        remote = source.remote
+    }
 }
