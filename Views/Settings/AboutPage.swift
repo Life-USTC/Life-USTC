8008f7a74b126b053f0b917fb0dbb75f90b6bda5
tiankaima <tiankaima@163.com>
2023-08-25 02:59:19 +0800

    chore: clean AboutPage

--- Views/Settings/AboutPage.swift
+++ Views/Settings/AboutPage.swift
@@ -8,24 +8,21 @@
 import SwiftUI
 import SwiftyJSON
 
+// The following GitHub does nothing than access GitHub's API to fetch contributor list.
 private let githubDumbTokenA = "github_pat_11AV4QBHQ0I8IdqcDkTkvH_"
 private let githubDumbTokenB =
     "gQyW7CGYb4OmlfJVApx3g7QsTo17d07SACsOAkpXkhBLN4NHZFZhg5zjWoy"
 
-extension Bundle {
-    var releaseNumber: String? {
-        infoDictionary?["CFBundleShortVersionString"] as? String
-    }
+private let links: [(label: String, url: String)] = [
+    ("GitHub:", "https://github.com/tiankaima/Life-USTC"),
+    ("Discord:", "https://discord.gg/BxdsySpkYP"),
+]
 
-    var buildNumber: String? { infoDictionary?["CFBundleVersion"] as? String }
-}
+let shareURL = URL(
+    string: "https://apps.apple.com/cn/app/life-ustc/id1660437438"
+)!
 
 struct AboutApp: View {
-    @Environment(\.openURL) private var openURL
-    let links: [(label: String, url: String)] = [
-        ("GitHub", "https://github.com/tiankaima/Life-USTC"),
-        ("Discord", "https://discord.gg/BxdsySpkYP"),
-    ]
     @State var contributorList: [(name: String, avatar: URL?)] = [
         (
             "tiankaima",
@@ -37,111 +34,142 @@ struct AboutApp: View {
         ),
     ]
 
+    var iconView: some View {
+        Image("Icon")
+            .resizable()
+            .frame(width: 200, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius: 10)
+            .contextMenu {
+                ShareLink(item: shareURL) {
+                    Label(
+                        "Share this app",
+                        systemImage: "square.and.arrow.up"
+                    )
+                }
+            }
+    }
+
+    var authorListView: some View {
+        HStack {
+            Text("Author:")
+                .font(.system(.title2, design: .monospaced, weight: .semibold))
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(contributorList, id: \.name) { contributor in
+                        AsyncImage(url: contributor.avatar) { image in
+                            image.resizable().aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 30, maxHeight: 30)
+                                .clipShape(Circle())
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        Text(contributor.name).fontWeight(.medium)
+                            .font(.title3)
+                    }
+                }
+            }
+        }
+    }
+
+    var linksView: some View {
+        VStack(alignment: .leading) {
+            ForEach(links, id: \.label) { link in
+                HStack {
+                    Text(link.label)
+                        .font(
+                            .system(
+                                .title2,
+                                design: .monospaced,
+                                weight: .semibold
+                            )
+                        )
+                    Text(link.url).foregroundColor(.gray)
+
+                    Spacer()
+                }
+            }
+            .frame(height: 40)
+        }
+    }
+
     var body: some View {
         VStack {
-            Spacer()
-            Image("Icon").resizable().frame(width: 200, height: 200)
-                .onTapGesture(count: 5) {
-                    UIPasteboard.general.string = String(
-                        describing: Array(
-                            UserDefaults.appGroup.dictionaryRepresentation()
-                        )
-                    )
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .contextMenu {
-                    ShareLink(item: "Life@USTC") {
-                        Label(
-                            "Share this app",
-                            systemImage: "square.and.arrow.up"
-                        )
-                    }
-                }
-                .shadow(radius: 10)
-            Spacer()
-            Text("Life@USTC").font(.title).bold()
+            iconView
+            Text("Life@USTC")
+                .font(.system(.title, weight: .bold))
+            Text(Bundle.main.versionDescription)
+                .font(.system(.caption, weight: .bold))
+                .foregroundColor(.secondary)
 
-            Text(
-                "Ver: \(Bundle.main.releaseNumber ?? "") build\(Bundle.main.buildNumber ?? "")"
+            Spacer()
+
+            authorListView
+            linksView
+        }
+        .padding()
+        .task {
+            onLoadFunc()
+        }
+        .navigationTitle("Title")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension AboutApp {
+    func onLoadFunc() {
+        Task {
+            var request = URLRequest(
+                url: URL(
+                    string:
+                        "https://api.github.com/repos/tiankaima/Life-USTC/contributors"
+                )!
             )
-            .font(.caption).bold().foregroundColor(.secondary)
+            request.httpMethod = "GET"
+            request.setValue(
+                "application/vnd.github+json",
+                forHTTPHeaderField: "Accept"
+            )
+            request.setValue(
+                "Bearer \(githubDumbTokenA + githubDumbTokenB)",
+                forHTTPHeaderField: "Authorization"
+            )
+            request.setValue(
+                "2022-11-28",
+                forHTTPHeaderField: "X-GitHub-Api-Version"
+            )
 
-            Spacer()
-
-            HStack {
-                Text("Author").fontWeight(.semibold).font(.title2)
-
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(contributorList, id: \.name) { contributor in
-                            AsyncImage(url: contributor.avatar) { image in
-                                image.resizable().aspectRatio(contentMode: .fit)
-                                    .frame(maxWidth: 30, maxHeight: 30)
-                                    .clipShape(Circle())
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            Text(contributor.name).fontWeight(.medium)
-                                .font(.title3)
-                        }
-                    }
-                }
+            let (data, response) = try await URLSession.shared.data(
+                for: request
+            )
+            if (response as! HTTPURLResponse).statusCode == 401 {
+                // Token is expired for some reason
+                return
             }
 
-            VStack(alignment: .leading) {
-                ForEach(links, id: \.label) { link in
-                    HStack {
-                        Text(link.label).fontWeight(.semibold).font(.title2)
-                            .padding([.top, .bottom], 2)
-                        Text(link.url).foregroundColor(.gray)
-                    }
-                }
-                .frame(height: 40)
-            }
-            .hStackLeading()
-        }
-        .padding().navigationBarTitle("About", displayMode: .inline)
-        .onAppear {
-            Task {
-                var request = URLRequest(
-                    url: URL(
-                        string:
-                            "https://api.github.com/repos/tiankaima/Life-USTC/contributors"
-                    )!
+            let dataJson = try JSON(data: data)
+            contributorList = dataJson.arrayValue.map {
+                (
+                    $0["login"].stringValue,
+                    URL(string: $0["avatar_url"].stringValue)
                 )
-                request.httpMethod = "GET"
-                request.setValue(
-                    "application/vnd.github+json",
-                    forHTTPHeaderField: "Accept"
-                )
-                request.setValue(
-                    "Bearer \(githubDumbTokenA + githubDumbTokenB)",
-                    forHTTPHeaderField: "Authorization"
-                )
-                request.setValue(
-                    "2022-11-28",
-                    forHTTPHeaderField: "X-GitHub-Api-Version"
-                )
-
-                let (data, response) = try await URLSession.shared.data(
-                    for: request
-                )
-                if (response as! HTTPURLResponse).statusCode == 401 {
-                    // Token is expired for some reason
-                    return
-                }
-                contributorList.removeAll()
-                let dataJson = try JSON(data: data)
-                for (_, userInfo) in dataJson {
-                    contributorList.append(
-                        (
-                            userInfo["login"].stringValue,
-                            URL(string: userInfo["avatar_url"].stringValue)
-                        )
-                    )
-                }
             }
         }
+    }
+}
+
+extension Bundle {
+    var releaseNumber: String? {
+        infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+
+    var buildNumber: String? {
+        infoDictionary?["CFBundleVersion"] as? String
+    }
+
+    var versionDescription: String {
+        "Ver: \(releaseNumber ?? "") build\(buildNumber ?? "")"
     }
 }
 
