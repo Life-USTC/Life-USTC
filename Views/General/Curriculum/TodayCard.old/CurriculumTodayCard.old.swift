0aadf1c8a5011156714468cc78cc96fd03abaee5
tiankaima <tiankaima@163.com>
2023-08-28 14:09:56 +0800

    feat: add back CurriculumTodayCard_old

---
+++ Views/General/Curriculum/TodayCard.old/CurriculumTodayCard.old.swift
@@ -0,0 +1,43 @@
+//
+//  CurriculumTodayCard.old.swift
+//  Life@USTC
+//
+//  Created by Tiankai Ma on 2023/8/28.
+//
+
+import SwiftUI
+
+struct CurriculumTodayCard_old: View {
+    @ManagedData(.curriculum) var curriculum: Curriculum
+
+    var _date: Date = .now
+
+    var date: Date { _date.stripTime() }
+
+    var todayLectures: [Lecture] {
+        curriculum.semesters.flatMap(\.courses).flatMap(\.lectures)
+            .filter {
+                (date ..< date.add(day: 1)).contains($0.startDate)
+            }
+            .sort()
+    }
+
+    var body: some View {
+        CurriculumTodayView_old(
+            lectures: todayLectures
+        )
+        .asyncStatusOverlay(_curriculum.status, text: "Curriculum") {
+            Button {
+                _curriculum.triggerRefresh()
+            } label: {
+                Label("Refresh", systemImage: "arrow.clockwise")
+                    .font(.caption)
+            }
+        }
+        .card()
+    }
+}
+
+#Preview{
+    CurriculumTodayCard_old()
+}
