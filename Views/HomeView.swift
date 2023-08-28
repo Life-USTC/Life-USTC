2e134ad79f6d4726eb9e70ed50e00f6396c32da7
tiankaima <tiankaima@163.com>
2023-08-28 14:09:56 +0800

    feat: add back CurriculumTodayCard_old

--- Views/HomeView.swift
+++ Views/HomeView.swift
@@ -13,6 +13,7 @@ struct HomeView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(spacing: 30) {
+                CurriculumTodayCard_old()
                 CurriculumTodayCard()
                 ExamPreviewCard()
                 CurriculumWeekCard()
