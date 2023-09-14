ee29c20df2472922c085d48b4b94a6026b0f53db
Odeinjul <odeinjul@gmail.com>
2023-09-14 20:37:01 +0800

    fix: update bus timetable

--- Schools/USTC/Delegates/USTC+BusDelegate.swift
+++ Schools/USTC/Delegates/USTC+BusDelegate.swift
@@ -29,8 +29,7 @@ class USTCBusDelegate: ManagedRemoteUpdateProtocol {
              reverse: true
          )
          */
-        var id = 1
-        var calendar = Calendar(identifier: .gregorian)
+        let calendar = Calendar(identifier: .gregorian)
         var from = "东区"
         var to = "高新"
         var type = "weekday"
@@ -38,21 +37,90 @@ class USTCBusDelegate: ManagedRemoteUpdateProtocol {
         var startTime = calendar.date(from: DateComponents (
             calendar: calendar,
             timeZone: TimeZone(identifier: "Asia/Shanghai"),
-            hour: 08,
-            minute: 00
+            hour: 6,
+            minute: 50
         ))
         var timeTable = [10, 0, 50]
-        result.append(
-            Bus (
-                id: id,
-                from: from,
-                to: to,
-                startTime: startTime!,
-                timeTable: timeTable,
-                type: type,
-                stationNum: stationNum
-            )
-        )
+        result.append(Bus (id: 1, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 70, to: startTime!)!
+        result.append(Bus (id: 2, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 280, to: startTime!)!
+        timeTable = [10, 0, 40]
+        result.append(Bus (id: 3, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 110, to: startTime!)!
+        timeTable = [10, 0, 45]
+        result.append(Bus (id: 4, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 235, to: startTime!)!
+        timeTable = [10, 0, 50]
+        result.append(Bus (id: 5, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 175, to: startTime!)!
+        timeTable = [10, 0, 30]
+        result.append(Bus (id: 6, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(byAdding: .minute, value: 45, to: startTime!)!
+        timeTable = [10, 0, 45]
+        result.append(Bus (id: 7, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        from = "高新"
+        to = "东区"
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 6, minute: 40))
+        timeTable = [5, 0, 40]
+        result.append(Bus (id: 8, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 8, minute: 00))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 9, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 9, minute: 35))
+        timeTable = [5, 0, 40]
+        result.append(Bus (id: 10, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 12, minute: 40))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 11, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 14, minute: 30))
+        timeTable = [5, 0, 50]
+        result.append(Bus (id: 12, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 18, minute: 25))
+        timeTable = [5, 0, 50]
+        result.append(Bus (id: 13, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 22, minute: 05))
+        timeTable = [5, 0, 40]
+        result.append(Bus (id: 14, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        from = "东区"
+        to = "高新"
+        type = "weekend"
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 07, minute: 00))
+        timeTable = [10, 0, 40]
+        result.append(Bus (id: 15, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 12, minute: 40))
+        timeTable = [10, 0, 40]
+        result.append(Bus (id: 16, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 18, minute: 30))
+        timeTable = [10, 0, 50]
+        result.append(Bus (id: 17, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 21, minute: 50))
+        timeTable = [10, 0, 50]
+        result.append(Bus (id: 18, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        from = "高新"
+        to = "东区"
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 08, minute: 00))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 19, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 13, minute: 40))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 20, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 16, minute: 00))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 21, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
+        startTime = calendar.date(from: DateComponents (calendar: calendar, timeZone: TimeZone(identifier: "Asia/Shanghai"), hour: 21, minute: 50))
+        timeTable = [5, 0, 45]
+        result.append(Bus (id: 22, from: from, to: to, startTime: startTime!, timeTable: timeTable, type: type, stationNum: stationNum))
+        
         
         return result
     }
--- Schools/USTC/Views/USTC+BusView.swift
+++ Schools/USTC/Views/USTC+BusView.swift
@@ -16,18 +16,73 @@ struct USTC_SchoolBusView: View {
             ForEach(buses) { bus in
                 VStack {
                     HStack {
-                        Text(bus.from)
+                        VStack (alignment: .leading){
+                            HStack (alignment: .bottom){
+                                Text(bus.from)
+                                    .fontWeight(.heavy)
+                                if (bus.from == "高新" && bus.to == "东区") {
+                                    Text("先研院")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                } else if (bus.from == "东区" && bus.to == "高新") {
+                                    Text("西区")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                                    
+                            }
+                            HStack (alignment: .top){
+                                Text(bus.startTime.stripHMwithTimezone())
+                                    .font(.caption)
+                                if (bus.from == "高新" && bus.to == "东区") {
+                                    Text(calendar.date(byAdding: .minute, value: 5, to: bus.startTime)! .stripHMwithTimezone())
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                } else if (bus.from == "东区" && bus.to == "高新") {
+                                    Text(calendar.date(byAdding: .minute, value: 10, to: bus.startTime)! .stripHMwithTimezone())
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                            }
+                        }
                         Spacer()
-                        Text(bus.to)
-                    }
-                    HStack {
-                        Text(bus.startTime.stripHMwithTimezone())
+                        Text("      ")
+                            .foregroundColor(.gray)
                         Spacer()
-                        Text(calendar.date(byAdding: .minute, value: bus.timeTable.reduce(0, +), to: bus.startTime)! .stripHMwithTimezone())
+                        VStack (alignment: .trailing){
+                            HStack (alignment: .bottom){
+                                Spacer()
+                                if (bus.from == "高新" && bus.to == "东区") {
+                                    Text("西区")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                } else if (bus.from == "东区" && bus.to == "高新") {
+                                    Text("先研院")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                                Text(bus.to)
+                                    .fontWeight(.heavy)
+                            }
+                            HStack {
+                                Spacer()
+                                Text("随停")
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                                Text(calendar.date(byAdding: .minute, value: bus.timeTable.reduce(0, +), to: bus.startTime)! .stripHMwithTimezone())
+                                    .font(.caption)
+                            }
+                        }
                     }
                 }
             }
         }
+        .refreshable {
+            _buses.triggerRefresh()
+        }
+        .navigationTitle("Bus Timetable")
     }
 }
 
