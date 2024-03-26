57a5c9920e3a579f3de5bf05575681287681111a
TianKai Ma <tiankaima@163.com>
2024-03-26 22:21:53 +0800

    Merge branch 'fix-curriculum-class'

--- Models/School/Curriculum/Course.swift
+++ Models/School/Curriculum/Course.swift
@@ -8,7 +8,7 @@
 import EventKit
 import Foundation
 
-struct Course: Codable, Identifiable, Equatable {
+class Course: Codable, Identifiable, Equatable {
     var id: Int = 0
     var name: String
     var courseCode: String
@@ -18,6 +18,78 @@ struct Course: Codable, Identifiable, Equatable {
     var description: String? = ""
     var credit: Double = 0
     var additionalInfo: [String: String] = [:]
+    
+    static func == (lhs: Course, rhs: Course) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case courseCode
+        case lessonCode
+        case teacherName
+        case lectures
+        case description
+        case credit
+        case additionalInfo
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        courseCode = try container.decode(String.self, forKey: .courseCode)
+        lessonCode = try container.decode(String.self, forKey: .lessonCode)
+        teacherName = try container.decode(String.self, forKey: .teacherName)
+        lectures = try container.decode([Lecture].self, forKey: .lectures)
+        description = try container.decode(String.self, forKey: .description)
+        credit = try container.decode(Double.self, forKey: .credit)
+        additionalInfo = try container.decode([String: String].self, forKey: .additionalInfo)
+        
+        for lecture in lectures {
+            lecture.course = self
+        }
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(courseCode, forKey: .courseCode)
+        try container.encode(lessonCode, forKey: .lessonCode)
+        try container.encode(teacherName, forKey: .teacherName)
+        try container.encode(lectures, forKey: .lectures)
+        try container.encode(description, forKey: .description)
+        try container.encode(credit, forKey: .credit)
+        try container.encode(additionalInfo, forKey: .additionalInfo)
+    }
+    
+    init(
+        id: Int = 0,
+        name: String,
+        courseCode: String,
+        lessonCode: String,
+        teacherName: String,
+        lectures: [Lecture],
+        description: String? = "",
+        credit: Double = 0,
+        additionalInfo: [String : String] = [:]
+    ) {
+        self.id = id
+        self.name = name
+        self.courseCode = courseCode
+        self.lessonCode = lessonCode
+        self.teacherName = teacherName
+        self.lectures = lectures
+        self.description = description
+        self.credit = credit
+        self.additionalInfo = additionalInfo
+        
+        for lecture in lectures {
+            lecture.course = self
+        }
+    }
 
     static let example = Course(
         name: "数学分析B1",
--- Models/School/Curriculum/Lecture.swift
+++ Models/School/Curriculum/Lecture.swift
@@ -9,11 +9,7 @@ import EventKit
 import SwiftUI
 
 /// Represent one lecture
-struct Lecture: Codable, Identifiable, Equatable {
-    var id: UUID {
-        UUID()
-    }
-
+class Lecture: Codable, Identifiable {
     var startDate: Date
     var endDate: Date
     var name: String
@@ -23,6 +19,68 @@ struct Lecture: Codable, Identifiable, Equatable {
     var additionalInfo: [String: String] = [:]
     var startIndex: Int?
     var endIndex: Int?
+    
+    var course: Course?
+    
+    enum CodingKeys: String, CodingKey {
+        case startDate
+        case endDate
+        case name
+        case location
+        case teacherName
+        case periods
+        case additionalInfo
+        case startIndex
+        case endIndex
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        startDate = try container.decode(Date.self, forKey: .startDate)
+        endDate = try container.decode(Date.self, forKey: .endDate)
+        name = try container.decode(String.self, forKey: .name)
+        location = try container.decode(String.self, forKey: .location)
+        teacherName = try container.decode(String.self, forKey: .teacherName)
+        periods = try container.decode(Double.self, forKey: .periods)
+        additionalInfo = try container.decode([String: String].self, forKey: .additionalInfo)
+        startIndex = try container.decodeIfPresent(Int.self, forKey: .startIndex)
+        endIndex = try container.decodeIfPresent(Int.self, forKey: .endIndex)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(startDate, forKey: .startDate)
+        try container.encode(endDate, forKey: .endDate)
+        try container.encode(name, forKey: .name)
+        try container.encode(location, forKey: .location)
+        try container.encode(teacherName, forKey: .teacherName)
+        try container.encode(periods, forKey: .periods)
+        try container.encode(additionalInfo, forKey: .additionalInfo)
+        try container.encodeIfPresent(startIndex, forKey: .startIndex)
+        try container.encodeIfPresent(endIndex, forKey: .endIndex)
+    }
+    
+    init(
+        startDate: Date,
+        endDate: Date,
+        name: String,
+        location: String  = "",
+        teacherName: String = "",
+        periods: Double = 0,
+        additionalInfo: [String : String] = [:],
+        startIndex: Int? = nil,
+        endIndex: Int? = nil
+    ) {
+        self.startDate = startDate
+        self.endDate = endDate
+        self.name = name
+        self.location = location
+        self.teacherName = teacherName
+        self.periods = periods
+        self.additionalInfo = additionalInfo
+        self.startIndex = startIndex
+        self.endIndex = endIndex
+    }
 
     static let example = Lecture(
         startDate: Date().stripTime() + DateComponents(hour: 7, minute: 50),
