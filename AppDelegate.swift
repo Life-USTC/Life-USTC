d66cfaa088c467d031f0b6382ce8c26c29785051
TianKai Ma <tiankaima@163.com>
2025-10-22 19:07:59 +0800

    fix: remove TPNS

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,53 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate,
     @LoginClient(.ustcCAS) var ustcCasClient: UstcCasClient
     @LoginClient(.ustcAAS) var ustcAASClient: UstcAASClient
 
-    #if IOS_SIMULATOR
-    func preparePreviews() {
-        InAppNotificationDelegate.shared.addInfoMessage("Preview debug message")
-    }
-    #endif
-
-    func shouldRunUpdate(on version: String) -> Bool {
-        #if IOS_SIMULATOR
-        // Update on developing: if previousVersion <= version
-        if let previousVersion = UserDefaults.appGroup.string(
-            forKey: "version"
-        ), previousVersion.versionCompare(version) == .orderedDescending {
-            return false
-        }
-        #else
-        // Update on release: if previousVersion < version
-        if let previousVersion = UserDefaults.appGroup.string(
-            forKey: "version"
-        ),
-            previousVersion.versionCompare(version) == .orderedDescending
-                || previousVersion.versionCompare(version) == .orderedSame
-        {
-            return false
-        }
-        #endif
-
-        print("Version <= \(version); Updating to version \(version)")
-        return true
-    }
-
     func startup() {
         _ustcCasClient.clearLoginStatus()
         _ustcAASClient.clearLoginStatus()
     }
 
-    #if IOS_SIMULATOR
-    // dummy definitions to avoid using TPNS service inside simulator
-    // as XCFramework lib isn't fully supported with Apple chips
-    func application(
-        _: UIApplication,
-        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        preparePreviews()
-        startup()
-        return true
-    }
-    #else
     func application(
         _: UIApplication,
         didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
@@ -67,5 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate,
         startup()
         return true
     }
-    #endif
 }
