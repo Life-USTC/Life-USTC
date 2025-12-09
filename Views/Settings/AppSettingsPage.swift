3fd3126700d501ed83d95fc11e74c1a5f52ce2b5
TianKai Ma <tiankaima@163.com>
2025-12-10 07:35:13 +0800

    fix: remove baidu since there's not any

--- Views/Settings/AppSettingsPage.swift
+++ Views/Settings/AppSettingsPage.swift
@@ -8,32 +8,9 @@
 import SwiftUI
 
 struct AppSettingsPage: View {
-    @AppStorage("scoreViewPreventScreenShot") var preventScreenShot = false
-    @AppStorage("appShouldNOTUpdateAnything", store: .appGroup) var appShouldNOTUpdateAnything = false
-    @AppStorage("useBaiduStatistics") var useBaiduStatistics = true
-    @AppStorage("widgetCanRefreshNewData", store: .appGroup) var widgetCanRefreshNewData: Bool? = nil
-
     var body: some View {
         List {
             Section {
-                Toggle(
-                    "Prevent screenshot when showing score",
-                    isOn: $preventScreenShot
-                )
-                Toggle(
-                    "Use Baidu Statistics",
-                    isOn: $useBaiduStatistics
-                )
-                Toggle(
-                    "Allow widget to refresh data",
-                    isOn: $widgetCanRefreshNewData ?? false
-                )
-                #if DEBUG
-                Toggle(
-                    "Disable update for everything",
-                    isOn: $appShouldNOTUpdateAnything
-                )
-                #endif
             } header: {
                 Text("General")
                     .textCase(.none)
